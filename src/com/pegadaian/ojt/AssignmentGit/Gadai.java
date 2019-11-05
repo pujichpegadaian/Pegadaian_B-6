942fbe9e46f5116ce5b9016ae8b2721040751bc3
bpratisia <57361112+bpratisia@users.noreply.github.com>
2019-11-05 20:53:44 +0700

    class gadai
    yang tadi salah input

--- src/com/pegadaian/ojt/AssignmentGit/Gadai.java
+++ src/com/pegadaian/ojt/AssignmentGit/Gadai.java
@@ -4,13 +4,94 @@ import java.util.Scanner;
 
 public class Gadai {
 	public static void main(String[] args) {
-	    Scanner input = new Scanner(System.in);
-	    double[] numbers = new double[5];
+			int menu;
+			int harga;
+			String nama;
+			String produk;
+			String detil;
+			boolean stNama = false;
+			boolean stKat = false;
+			boolean stDesk = false;
+			boolean stPrice = false;
+			
+			boolean loop = true;
 
-	    for (int i = 0; i < numbers.length; i++)
-	    {
-	        System.out.println("Please enter number");
-	        numbers[i] = input.nextDouble();
-	    }
+			Scanner input = new Scanner(System.in);
+			while(loop) {
+				try {
+					System.out.println("Name : ");
+					nama = input.nextLine();
+					if ((nama.length()>3 )&&(nama.length()<15) ) {
+						stNama=true;
+						loop=false;
+					}else {
+						System.out.println("Input minimal 3 karakter dan kurang dari 15 karakter");
+					}
+				} catch (Exception e) {
+					System.out.println();
+					input.next();
+					continue;
+				}
+			}
+			loop=true;
+
+			while(loop) {
+				try {
+					System.out.println("Product Category : ");
+					produk = input.nextLine();
+					if ((produk.equalsIgnoreCase("laptop"))||(produk.equalsIgnoreCase("motor"))||(produk.equalsIgnoreCase("emas"))) {
+						stNama=true;
+						loop=false;
+					}else {
+						System.out.println("Barang gadai harus diantara barang berikut : Laptop, Motor, Emas");
+					}
+				} catch (Exception e) {
+					System.out.println();
+					input.next();
+					continue;
+				}
+			}
+			loop=true;
+
+			while(loop) {
+				try {
+					System.out.println("Description : ");
+					detil = input.nextLine();
+			        int countWords=0;
+			        for(int i=0; i<detil.length()-1; i++)
+			        {
+			            if(detil.charAt(i)==' ' && detil.charAt(i+1)!=' ')
+			                countWords++;
+			        }
+			        if (countWords < 2) {
+				        System.out.println("Deskripsi harus memiliki minimal 3 kata");
+			        } else {
+			        	stDesk=true;
+						loop=false;
+			        }
+				} catch (Exception e) {
+					System.out.println();
+					input.nextLine();
+					continue;
+				}
+			}
+			loop=true;
+			
+
+			while(loop) {
+				try {
+					System.out.println("Price : ");
+					harga = input.nextInt();
+					loop=false;
+				} catch (Exception e) {
+					System.out.println("Masukkan angka");
+					input.next();
+					continue;
+				}
+			}
+			loop=true;
+			
+//			array[array.length]= new database [produk, nama, harga, harga, "gadai", detil]
+
 	}
 }
