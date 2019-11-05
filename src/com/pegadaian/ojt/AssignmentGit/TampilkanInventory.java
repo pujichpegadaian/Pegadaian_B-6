7f825a9961e1693b6085c0937db5fdd9e4c3b697
shantierq <57378076+shantierq@users.noreply.github.com>
2019-11-05 23:39:33 +0700

    tampilan inventory new
    new tampilan inventory

--- src/com/pegadaian/ojt/AssignmentGit/TampilkanInventory.java
+++ src/com/pegadaian/ojt/AssignmentGit/TampilkanInventory.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class TampilkanInventory {
 //	public static void main(String[] args) {
-	public void tampilkan (ArrayList data) {		
+	public void tampilkan (ArrayList<Database> data) {		
 			
 	//	Database database = new Database();/
 //		Database dataGadai = new Database (data.size()+1, produk, nama, harga, harga,);
 //		data.add(dataGadai);
 		
 		System.out.println("===============================================");
-		String Format = "%-3 | %-15s | %-5s | %-15s | %-10d |%n";
+		String Format = "%-3s | %-15s | %-5s | %-15s | %-10s |%n";
 		System.out.format(Format, "ID", "Nama", "Product", "Description", "Price" );
 		System.out.println("===============================================");
 		for(int i=0; i<data.size(); i++)
         {
 			
 //			System.out.println(data.get(i).getID()+" nama :"+data.get(i).getNama());
-//			String Format2 = "%-3 | %-15s | %-5s | %-15s | %-10d |%n";
+			String Format2 = "%-3d | %-15s | %-5s | %-15s | %-10d |%n";
 	        System.out.format(Format, data.get(i).getID(), data.get(i).getNama(), data.get(i).getProduct(), data.get(i).getDescription(), data.get(i).getPrice());
         }
 		System.out.println("===============================================");
