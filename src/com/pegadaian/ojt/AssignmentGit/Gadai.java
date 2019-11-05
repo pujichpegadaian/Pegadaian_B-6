c1524252f1bb399178bf0d1c1636822a2b181678
bpratisia <57361112+bpratisia@users.noreply.github.com>
2019-11-05 20:52:45 +0700

    class Gadai
    class Gadai
    input nama
    input produk
    input detil
    input harga
    
    belum bisa store data

---
+++ src/com/pegadaian/ojt/AssignmentGit/Gadai.java
@@ -0,0 +1,16 @@
+package com.pegadaian.ojt.tugas;
+
+import java.util.Scanner;
+
+public class Gadai {
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    double[] numbers = new double[5];
+
+	    for (int i = 0; i < numbers.length; i++)
+	    {
+	        System.out.println("Please enter number");
+	        numbers[i] = input.nextDouble();
+	    }
+	}
+}
